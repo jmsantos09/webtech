dcb53fb53a0deb668d62d6a22a1e77c0612b123b
jmsantos09 <132550989+jmsantos09@users.noreply.github.com>
2024-01-16 00:20:46 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.1">
+    <title>Apple Store</title> 
+    <link rel="stylesheet"  href = "Style.css"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head> 
+<body>
+
+<div class="header">
+    <div class="container">
+
+    </div>
+    <div class="navbar">
+        <div class="logo">
+            <a href="Index.html"><img src="image/logo.png" width="125px"></a>
+        </div>
+        <nav>
+            <ul id="MenuItems">
+                <li><a href="Index.html">Home</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Accessories</a></li>
+                <li><a href="login.php">Account</a></li>
+            </ul>
+        </nav>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-2">
+            <h1>The Best Way to Buy the<br> Products you Love</h1>
+            <p>Think Different — But Not Too Different</p>
+        </div>
+        <div class="col-2">
+            <img src="Image/homepic.png">
+            
+        </div>
+    </div>
+</div>
+</div> 
+
+<!--------Featured products-------->
+<div class="small-container">
+    <h2 class="title">Featured Products</h2>
+    <div class="row">
+        <div class="col-4">
+            <a href="Products details.html"><img src="Image/img1.jpg"></a>
+            <a href="Products details.html"><h4>Iphone 11 Purple</h4></a>
+            <p>₱47,990</p>
+
+        </div>
+        <div class="col-4">
+            <a href="Product details(1).html"><img src="Image/img2.jpg"></a>
+            <a href="Product details(1).html"><h4>Iphone 11 Mint Green</h4></a>
+            <p>₱47,990</p>
+
+        </div>
+        <div class="col-4">
+            <a href="Product detail(2).html"><img src="Image/img3.jpg"></a>
+            <a href="Product detail(2).html"><h4>Iphone 11 Red</h4></a>
+            <p>₱47,990</p>
+        </div>
+    </div>
+</div>
+
+
+<!----------footer-------->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Download Our App</h3>
+                <p>Download App for Andriod and ios mobile phones</p>
+                <div class="app-logo">
+                    <img src="Image/logo3.png">
+                </div>
+            </div>
+            <div class="footer-col-2">
+                <img src="Image/logo2.png">
+                <p>Apple Inc. designs, manufactures and markets smartphones, personal computers, 
+                    tablets, wearables and accessories,
+                     and sells a variety of related services.</p>
+            </div>
+            <div class="footer-col-3">
+                <h3>Useful Links</h3>
+                <ul>
+                    <li>Apple Discord Community</li>
+                    <li> Apple Store Blog Post</li>
+                    <li>Apple Store</li></a>
+                </ul>
+            </div>
+            <div class="footer-col-4">
+                <h3>Follow us</h3>
+                <ul>
+                    <a href=""><li>Facebook</li></a>
+                    <a href=""><li>Instagram</li></a>
+                    <a href=""><li>Twitter</li></a>
+                    <a href=""><li>YouTube</li></a>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <p class="Copyright">Copyright 2024</p>
+    </div>
+</div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,55 @@
+<?php
+require 'db.php';
+    if(isset($_POST["submit"])){
+        $name = $_POST["name"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password  = $_POST["password"];
+        $confirmpassword = $_POST["confirmpassword"];
+        $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+        if(mysqli_num_rows($duplicate) > 0){
+            echo
+            "<script> alert('Username or Email Has already Taken'); </script>";
+        }
+        else{
+            if($password == $confirmpassword){
+                $query = "INSERT INTO tb_user VALUES('','$name','$username','$email','$password')";
+                mysqli_query($conn,$query);
+                echo
+                "<script> alert('Registration Successful');</script>";
+            }
+            else{
+                echo
+                "<script> alert('Password Does Not Match');</script>";
+            }
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h2>Registration</h2>
+    <form class="" action="" method="post" autocomplete="off">
+    <label for="name">Name: </label>
+    <input type="text" name="name" id="name" required value =""><br>
+    <label for="username">Username: </label>
+    <input type="text" name="username" id="username" required value =""><br>
+    <label for="email">Email: </label>
+    <input type="text" name="email" id="email" required value =""><br>
+    <label for="password">Password: </label>
+    <input type="text" name="password" id="password" required value =""><br>
+    <label for="confirmpassword">Confirm Password: </label>
+    <input type="text" name="confirmpassword" id="password" required value =""><br>
+    <button type="submit" name="submit">Register</button>
+    </form>
+    <br>
+    <a href="login.php">Login</a>
+</body>
+</html>
